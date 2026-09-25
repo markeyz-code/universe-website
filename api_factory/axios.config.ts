b71a2add4b2cdbf341666f4f4bf965650719020b
Marquis <abahmarquis@gmail.com>
2026-09-25 15:54:01 +0100

    fghjkl

--- api_factory/axios.config.ts
+++ api_factory/axios.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getBaseUrl = () => {
   return process.env.NODE_ENV === 'production'
-    ? 'https://api.interntional.medlabconvo.com/api/v1'
+    ? process.env.NUXT_PUBLIC_API_BASE
     : 'http://localhost:4000/api/v1';
 };
 
