8d3ad761266595175ae39453932c9e76c05ce088
Marquis <abahmarquis@gmail.com>
2026-09-26 08:25:14 +0100

    chore: fix prerender routes and clear cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      ignore: ['/events']
+    }
+  },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   app: {
